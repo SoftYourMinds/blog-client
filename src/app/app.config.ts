29c5200cf0a80ba8c3aa9e3fde9af615f7114377
Daniil Shkarupa <zshkarrr@gmail.com>
2024-01-19 01:58:50 +0200

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes)], 
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CoreComponent } from './core/core.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'blog', component: CoreComponent},
+    { path: '', pathMatch: 'full', redirectTo: 'blog' } 
+];
+
+
+
