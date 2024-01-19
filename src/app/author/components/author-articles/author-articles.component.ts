ac659d4802a0d2404fcc57e3d531756055f7ca30
Daniil Shkarupa <zshkarrr@gmail.com>
2024-01-20 00:06:13 +0200

    created main page

---
+++ src/app/author/components/author-articles/author-articles.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-author-articles',
+  standalone: true,
+  imports: [],
+  templateUrl: './author-articles.component.html',
+  styleUrl: './author-articles.component.scss'
+})
+export class AuthorArticlesComponent {
+
+}
